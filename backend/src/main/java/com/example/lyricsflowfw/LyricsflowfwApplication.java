8cc4c647096ab27c760cd0ce59cc38316306b336
Julia Guilhermino <juh.guilhermino03@gmail.com>
2026-07-04 15:49:27 -0300

    chore: atualização das dependencias no pom.xml

---
+++ backend/src/main/java/com/example/lyricsflowfw/LyricsflowfwApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyricsflowfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsflowfwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsflowfwApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/lyricsflowfw/LyricsflowfwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lyricsflowfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsflowfwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
